d0c62e2a0ac7fd482a4880f6ed3a9d2e192525ff
Lelo <heloisacmk@hotmail.com>
2023-04-05 18:13:58 -0700

    Campground New & Create

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -33,16 +34,23 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    //without parse.body we can't see a thing =P
+    //res.send(req.body);
+    //Ok, after looking what req.body was, lets create a new Camground and save
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
     //we findById to get exacly the one from the loop in the index.ejs file
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground})
-})
-
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping'});
-    await camp.save();
-    res.send(camp);
+    res.render('campgrounds/show', { campground })
 });
 
 app.listen(3000, () => {
