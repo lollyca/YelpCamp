33462d1c33bde333c44ca4cc531ad7aec831b299
Lelo <heloisacmk@hotmail.com>
2023-05-01 12:41:58 -0700

    Serving Static Assests: public folder

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+// const Joi = require('joi');
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const ExpressError = require('./utils/ExpressError');
 
-const Joi = require('joi');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -15,7 +15,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
     // userCreateIndex: true, - its no longer supported option
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 //after mongoose.connecet we connect the db mongo with mongoose and check if we have errors
@@ -34,6 +35,7 @@ app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
