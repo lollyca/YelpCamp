20d6935811b88285f078dee8d09b87fc1c1518d6
Lelo <heloisacmk@hotmail.com>
2023-05-03 11:20:44 -0700

    Adding Author to campground model & re-seeding db

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
+            author: "6451c39072cb87b8d72c3482",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
