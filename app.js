0c927eba96e51a13c2dfddec465f2060126e77f0
Lelo <heloisacmk@hotmail.com>
2023-05-02 19:17:15 -0700

    Register Route Logic

--- app.js
+++ app.js
@@ -63,6 +63,8 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //*
 app.use((req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-// const catchAsync = require('../utils/catchAsync');
+const cathcAsync = require('../utils/cathAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+router.post('/register', cathcAsync(async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports = router;
