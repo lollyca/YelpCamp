7c807efb0dcf9be298298ac1c5ee0f5b31538212
Lelo <heloisacmk@hotmail.com>
2023-05-05 09:30:50 -0700

    Refactoring To Camgrounds Controller

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,73 +1,36 @@
 const { isLoggedIn, isAuthor, validatedCampground } = require('../middleware');
 const cathcAsync = require('../utils/cathAsync');
 const Campground = require('../models/campground');
+const campgrounds = require('../controllers/campgrounds');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', cathcAsync(async (req, res) => {
+router.get('/', cathcAsync(campgrounds.index));
 
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}));
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new');
-});
+router.post('/', isLoggedIn, validatedCampground, cathcAsync(campgrounds.createCampground));
 
-router.post('/', isLoggedIn, validatedCampground, cathcAsync(async (req, res, next) => {
-    //without parse.body we can't see a thing =P
-    //res.send(req.body);
-    //Ok, after looking what req.body was, lets create a new Camground and save
-    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground');
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.get('/:id', cathcAsync(campgrounds.showCampground));
 
-router.get('/:id', cathcAsync(async (req, res) => {
-    //we findById to get exacly the one from the loop in the index.ejs file
-    const campground = await Campground.findById(req.params.id)
-        .populate({
-            path: 'reviews',
-            populate: {
-                path: 'author'
-            }
-        })
-        .populate('author');
-    console.log(campground);
-    if (!campground) {
-        req.flash('error', 'Campground Not Found');
-        res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', { campground });
-}));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, cathcAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground });
-}));
+router.get('/:id/edit', isLoggedIn, isAuthor, cathcAsync(campgrounds.renderEditForm));
 
 //#update
-router.put('/:id', isLoggedIn, validatedCampground, cathcAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground');
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.put('/:id', isLoggedIn, validatedCampground, cathcAsync(campgrounds.updateCampground));
 
-router.delete('/:id', isLoggedIn, cathcAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted Campground');
-    res.redirect('/campgrounds');
-}));
+router.delete('/:id', isLoggedIn, cathcAsync(campgrounds.deleteCampground));
 
 module.exports = router;
 
 
-//Learning:
-//We use async functions when we need to look up our database first to continue doing things
-//#update  ---  After seting up the route for edit we need to ACTUALLY update the thing, thats why we have #update right bellow
+/*Learning:
+We use async functions when we need to look up our database first to continue doing things
+
+#update  ---  After seting up the route for edit we need to ACTUALLY update the thing, thats why we have #update right bellow
+
+#2:
+without parse.body we can't see a thing =P
+Ok, after looking what req.body was, lets create a new Camground and save
+if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+*/
 
