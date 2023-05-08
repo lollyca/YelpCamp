c6679cd112faeb59a15062c39418998a9b1a8027
Lelo <heloisacmk@hotmail.com>
2023-05-08 10:23:54 -0700

    Deleting Images from mongoDB

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -14,7 +14,7 @@ module.exports.renderNewForm = (req, res) => {
 //Learning #2
 module.exports.createCampground = async (req, res, next) => {
     const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
@@ -50,9 +50,13 @@ module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs); // = don't pass the array, just take the data from it =] 
     await campground.save();
+    if (req.body.deleteImages) {
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+        console.log(campground);
+    };
     req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`);
 };
