a25f9204e1f4b301744c56354b80b2d7a002580c
Lelo <heloisacmk@hotmail.com>
2023-05-08 11:15:10 -0700

    Adding a Thumbnail Virtual

--- models/campground.js
+++ models/campground.js
@@ -2,18 +2,22 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
+    images: [ImageSchema],
     name: String,
     price: Number,
     description: String,
     location: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
@@ -24,7 +28,7 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-})
+});
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
@@ -34,7 +38,7 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
             }
         })
     }
-})
+});
 
 const Camp = mongoose.model('Camp', CampgroundSchema);
 module.exports = Camp;
